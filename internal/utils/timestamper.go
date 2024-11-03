822e64bb277785003595108900a64f922ffe0284
vg_varankin <22920694+SversusN@users.noreply.github.com>
2024-11-03 22:29:37 +0300

    no need

--- internal/utils/timestamper.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "time"
-
-// GetTimeStamp возвращает unix ts
-func GetTimeStamp(t time.Time) int64 {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix()
-}
